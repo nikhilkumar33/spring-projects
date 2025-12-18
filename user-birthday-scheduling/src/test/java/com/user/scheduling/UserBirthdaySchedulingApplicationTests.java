081cb9bcf48d2ad491dee1ea1bab0dd446bc6173
nikhilkumar33 <nikhilkumarjena33@gmail.com>
2025-12-18 16:16:47 +0530

    demo change 2

---
+++ user-birthday-scheduling/src/test/java/com/user/scheduling/UserBirthdaySchedulingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.scheduling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBirthdaySchedulingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
